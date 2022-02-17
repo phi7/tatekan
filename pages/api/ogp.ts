2971443a8d22b04dff17b664c0eac36876ae15f6
nt <kumypc@gmail.com>
2022-02-17 13:23:41 +0900

    second10

--- pages/api/ogp.ts
+++ pages/api/ogp.ts
@@ -7,8 +7,8 @@ const createOgp = async (
   res: NextApiResponse
 ): Promise<void> => {
   const { id } = req.query;
-  const WIDTH = 1200 as const;
-  const HEIGHT = 630 as const;
+  const WIDTH = 600 as const;
+  const HEIGHT = 315 as const;
   const canvas = createCanvas(WIDTH, HEIGHT);
   const ctx = canvas.getContext("2d");
   registerFont(path.resolve("./fonts/NikkyouSans-mLKax.ttf"), {
@@ -19,14 +19,14 @@ const createOgp = async (
   //背景を描写
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
   //フォントを指定
-  ctx.font = "60px NikkyouSans-mLKax";
+  ctx.font = "45px NikkyouSans-mLKax";
   //文字の色を指定
   ctx.fillStyle = "#FFFFFF";
   //文章の位置を指定
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   //文章の内容を決めた
-  const text = "入力した文字は「" + String(id) + "」なのねん";
+  const text = String(id);
   //文章を描写
   ctx.fillText(text, WIDTH / 2, HEIGHT / 2);
   //buffer型にすることでバイナリデータとする
